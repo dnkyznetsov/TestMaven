c7cf966fc73a576b0a6bbd815e060651bcf14ed8
dnkyznetsov <dnkyznetsov@gmail.com>
2015-08-13 15:37:55 +0400

    first commit

---
+++ src/main/java/main.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+/**
+ * Created by dima on 13.08.2015.
+ */
+public class main {
+
+    public static void main( String[] args ) throws IOException {
+        System.out.println( "Hello World" );
+        System.in.read();
+    }
+
+}
